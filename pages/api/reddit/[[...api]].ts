9323331da0edd2c14aeea22b711106d6b90c978c
hrishi045 <hrishi045@gmail.com>
2021-02-28 01:15:19 +0530

    Refactor and make snoowrap a singleton

--- pages/api/reddit/[[...api]].ts
+++ pages/api/reddit/[[...api]].ts
@@ -1,7 +1,7 @@
 import { getSession } from 'next-auth/client'
 import { getToken } from 'next-auth/jwt'
-import { NextApiRequest, NextApiResponse } from 'next-auth/_utils'
-import snoowrap from 'snoowrap'
+import { NextApiRequest, NextApiResponse } from 'next'
+import getSnoowrap from '../../../utils/api/snoowrap'
 import type { JWTToken } from '../auth/[...nextauth]'
 
 const secret = process.env.SECRET
@@ -12,18 +12,24 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
   const qs = new URLSearchParams()
 
+  if (!session || !token) {
+    res.send({
+      error: 'You must be sign in to view the protected content on this page.',
+    })
+    return
+  }
+
   for (const [key, value] of Object.entries(req.query)) {
     if (key != 'api') qs.set(key, value as string)
   }
 
-  const uri = (req.query.api as string[]).join('/')
+  const uri = (req.query.api as string[]).join('/') as string
 
-  const r = new snoowrap({
-    userAgent: 'Resium/1.0',
-    clientId: process.env.REDDIT_CLIENT_ID,
-    clientSecret: process.env.REDDIT_CLIENT_SECRET,
-    refreshToken: token.refreshToken,
-  })
+  if (uri.includes('undefined')) {
+    res.send('null')
+  }
+
+  const r = await getSnoowrap(req)
 
   const data = await r.oauthRequest({
     uri: `${uri}${qs.toString().length > 0 ? '?' + qs.toString() : ''}`,
@@ -31,14 +37,5 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     method: req.method,
   })
 
-  if (!session) {
-    res.send({
-      error: 'You must be sign in to view the protected content on this page.',
-    })
-    return
-  }
-
-  // r.getMe().then((_) => {
   res.send(data)
-  // })
 }
