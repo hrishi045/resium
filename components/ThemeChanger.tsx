78ee3c631d8314a61e36396d7b8fe1796cf5cf35
hrishi045 <hrishi045@gmail.com>
2021-02-28 01:12:32 +0530

    Theme changer no longer fixed

--- components/ThemeChanger.tsx
+++ components/ThemeChanger.tsx
@@ -1,15 +1,37 @@
 import { useTheme } from 'next-themes'
+import cx from 'classnames'
 
 export default function ThemeChanger() {
-  const { theme, setTheme } = useTheme()
+  const { setTheme } = useTheme()
 
   return (
-    <div className="fixed flex justify-around w-full gap-2 border-b border-gray-500 bg-gray-50 dark:bg-gray-800">
-      The current theme is: {theme}
-      <br />
-      <button onClick={() => setTheme('light')}>Light Mode</button>
-      <button onClick={() => setTheme('dark')}>Dark Mode</button>
-      <button onClick={() => setTheme('system')}>System Default</button>
+    <div className={cx('bg-gray-50 dark:bg-gray-800')}>
+      <div className="flex justify-around max-w-4xl gap-2 px-3 py-2 mx-auto text-xs font-semibold text-white capitalize">
+        <button
+          className={cx(
+            'px-4 py-2  bg-gray-700 rounded-sm hover:bg-gray-300 hover:text-gray-800'
+          )}
+          onClick={() => setTheme('light')}
+        >
+          Light Mode
+        </button>
+        <button
+          className={cx(
+            'px-4 py-2  bg-gray-700 rounded-sm hover:bg-gray-300 hover:text-gray-800'
+          )}
+          onClick={() => setTheme('dark')}
+        >
+          Dark Mode
+        </button>
+        <button
+          className={cx(
+            'px-4 py-2  bg-gray-700 rounded-sm hover:bg-gray-300 hover:text-gray-800'
+          )}
+          onClick={() => setTheme('system')}
+        >
+          System Default
+        </button>
+      </div>
     </div>
   )
 }
