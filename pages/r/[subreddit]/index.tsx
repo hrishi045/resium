db0036f74a6adb8a8f7c52d7585fedcf3e1670be
hrishi045 <hrishi045@gmail.com>
2021-02-28 01:15:51 +0530

    Refactor these to make use of PageListing layout

--- pages/r/[subreddit]/index.tsx
+++ pages/r/[subreddit]/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import { useRouter } from 'next/router'
 import { useSubreddit } from '../../../redditapi/hooks'
-import Head from 'next/head'
-import Link from 'next/link'
+import PostListing from '../../../components/PostListing'
 
 const Router = () => {
   const {
@@ -11,32 +10,7 @@ const Router = () => {
 
   const { data, isLoading, isError } = useSubreddit({ id: subreddit as string })
 
-  if (isError) return <p>Failed to fetch post</p>
-  if (isLoading) return <p>Loading post &hellip;</p>
-
-  return (
-    <div>
-      <Head>
-        <title>r/{subreddit}</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <div>
-          <h1 className="text-lg font-medium">r/{subreddit}</h1>
-        </div>
-        {data.map((post) => (
-          <div className="post" key={post.name}>
-            <p>
-              <Link href={post.permalink}>
-                <a>{post.title}</a>
-              </Link>
-            </p>
-          </div>
-        ))}
-      </main>
-    </div>
-  )
+  return <PostListing data={data} isLoading={isLoading} isError={isError} />
 }
 
 export default Router
--- pages/u/[username]/index.tsx
+++ pages/u/[username]/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import { useRouter } from 'next/router'
 import { useUser } from '../../../redditapi/hooks'
-import Head from 'next/head'
-import Link from 'next/link'
+import PostListing from '../../../components/PostListing'
 
 const Router = () => {
   const {
@@ -11,32 +10,7 @@ const Router = () => {
 
   const { data, isLoading, isError } = useUser({ id: username as string })
 
-  if (isError) return <p>Failed to fetch user</p>
-  if (isLoading) return <p>Loading user...</p>
-
-  return (
-    <div>
-      <Head>
-        <title>u/{username}</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <div>
-          <h1 className="text-lg font-medium">u/{username}</h1>
-        </div>
-        {data.map((post) => (
-          <div className="post" key={post.name}>
-            <p>
-              <Link href={post.permalink}>
-                <a>{post.title}</a>
-              </Link>
-            </p>
-          </div>
-        ))}
-      </main>
-    </div>
-  )
+  return <PostListing data={data} isError={isError} isLoading={isLoading} />
 }
 
 export default Router
